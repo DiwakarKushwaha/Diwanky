ec93f002ff7d95e308f55b7bda0d60b579c8e0d6
DiwakarKushwaha <99752642+DiwakarKushwaha@users.noreply.github.com>
2024-04-01 02:58:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,22 @@ const quizDB =[
         c:"Kalinga",
         d:"Magadha",
         ans:"ans1",
-        detail:"<hr> hghjggggggggggggggggggggggggggggggggggggggggggggggggggk,,,,,,"
+        detail:"<hr> The correct answer is Andhra:- Dhanyakataka (also known as Dharanikota) is located in the present-day Andhra Pradesh state of India. It was a prominent Buddhist center under the Mahasanghikas. It is also believed that the Great Stupa at Amaravati, which is one of the most significant Buddhist monuments in India, was situated in the same region. This monument is a fine example of Buddhist art and architecture, further emphasizing the importance of this region as a Buddhist center. In contrast, Gandhara, located in present-day Pakistan and Afghanistan, was also a major center of Buddhist art and culture, but it iss not the region where Dhanyakataka was located. Gandhara is especially known for the Gandhara style of Buddhist art, which incorporated influences from Greek and Roman art."
+    },
+    {
+        question: "With reference to ancient India, consider the following statements :",
+        e:"",
+        f:"1. The concept of Stupa is Buddhist in origin.",
+        g:"2. Stupa was generally a repository of relics.",
+        h:"3. Stupa was a votive and commemorative structure in Buddhist tradition.",
+        i:"",
+        j:"How many of the statements given above are correct?",
+        a:"Only one",
+        b:"Only two",
+        c:"All three",
+        d:"None",
+        ans:"ans2",
+        detail:"<hr>The correct answer is Only two:- Stupa is a pre-Buddhist tumuli in which sramaṇas were buried in a seated position called chaitya. The term “stupa” comes from the Sanskrit word “stupa,” which means “heap” or “pile.” Originally, stupas were simple mounds of earth or stones that served as commemorative markers for important events or burial sites. Hence statement 1 is incorrect. One of the primary purposes of a Stupa was to serve as a repository for relics (sarira) associated with the Buddha or other significant Buddhist figures. These relics were often placed in a central chamber within the Stupa. Hence statement 2 is correct. Besides housing relics, Stupas were often erected to commemorate significant events in the life of the Buddha, such as his birth, enlightenment, first sermon, and death. They also served as a focus for devotional practice. Hence statement 3 is correct."
     },
     {
         question: "With reference to ancient South India, Korkai, Poompuhar and Muchiri were well known as",
@@ -27,7 +42,7 @@ const quizDB =[
         c:"centres of iron-and-steel making",
         d:"shrines of Jain Tirthankaras",
         ans:"ans2",
-        detail:""
+        detail:"<hr> The correct answer is Ports:- Korkai, Poompuhar, and Muchiri (Muziris) were all well-known ancient ports in South India. Korkai:- Located in present-day Tamil Nadu, Korkai was a major port and the capital of the early Pandyan Kingdom. It was known for its pearl fishery and was a central hub for trade with the Roman Empire. Poompuhar:- Also known as Kaveripoompattinam, it was a significant port city in the Chola kingdom. Located at the mouth of the river Kaveri, it served as an important maritime and trade center. Muchiri (Muziris):- Located near present-day Kodungalloor in Kerala, Muziris was one of the major ports on the Malabar Coast during ancient times. It was a vital link in the maritime trade routes connecting the Mediterranean and the Red Sea to the Indian Ocean and was famed for its spice trade. Therefore, these cities were not capital cities (though Korkai was an early capital of the Pandyan Kingdom, it is more known as a port), they were not centers of iron-and-steel making, and they were not known as shrines of Jain Tirthankaras. They were best known as important ports facilitating extensive trade."
 
     },
     {
@@ -43,7 +58,7 @@ const quizDB =[
         c:"Young girls keeping watch over agricultural fields and driving away birds and animals",
         d:"A king defeated in a battle committing ritual suicide by starving himself to death",
         ans:"ans4",
-        detail:""
+        detail:"<hr>The correct answer is Option 4:- Sangam poems are pervaded with a warrior ethic. The goal of the hero of the Puram poems was pukal (glory, fame) and a heroic death was greatly valued. It was believed that the spirit of a warrior who died in battle dwelt in paradise. A poem in the Purananuru suggests that the bodies of warriors who did not die in battle were cut with swords before the funerary rites, to simulate death in battle. The practice of vattakirutal was one in which a defeated king committed ritual suicide by starving himself to death, accompanied by those who had been close to him during his lifetime. The worship of memorial stones (natukal) was a corollary of the importance of the heroic ideal. Memorial stones were erected in honor of heroes who died fighting valiantly in battle; the spirit of the fallen hero was believed to reside in these stones. "
     },
     {
         question: "Consider the following dynasties:",
@@ -58,22 +73,22 @@ const quizDB =[
         c:"Only three",
         d:"None",
         ans:"ans4",
-        detail:""
+        detail:"<hr>The correct answer is None:- Hoysala Dynasty:-This dynasty ruled most of the modern-day state of Karnataka, India, between the 10th and 14th centuries.The capital of the Hoysalas was initially located at Belur but was later moved to Halebidu. Gahadavala Dynasty:- This dynasty ruled the Gangetic plains of North India during the late 11th and early 12th century AD. Their capital was located at Kanyakubja (present-day Kannauj). Kakatiya Dynasty:- This dynasty was a South Indian dynasty that ruled most of the eastern Deccan region comprising present-day Telangana and Andhra Pradesh, and parts of eastern Karnataka and southern Odisha between the 12th and 14th centuries. Their capital was Orugallu, now known as Warangal. Yadava Dynasty:- The Yadavas of Devagiri ruled a kingdom in central India around the modern city of Aurangabad in Maharashtra from the 9th century until the early 14th century. None of the listed dynasties established their kingdoms in the early eighth century AD."
     },
     {
         question: "With reference to ancient Indian History, consider the following pairs:",
-        e:"Literary work&emsp; &emsp; &emsp; &emsp; &emsp;Author",
-        f:"",
-        g:"",
-        h:"",
-        i:"",
-        j:"",
-        a:"Andhra",
-        b:"Gandhara",
-        c:"Kalinga",
-        d:"Magadha",
-        ans:"ans1",
-        detail:""
+        e:" &nbsp;Literary work&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;Author",
+        f:"1: Devichandragupta&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;Bilhana",
+        g:"2: Hammira-Mahakavya Nayachandra &emsp; &emsp;Suri",
+        h:"3: Milinda-panha&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; Nagarjuna",
+        i:"4: Nitivakyamrita &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; Somadeva Suri",
+        j:"How many of the above pairs are correctly matched?",
+        a:"Only one",
+        b:"Only two",
+        c:"Only three",
+        d:"All four",
+        ans:"ans3",
+        detail:"The correct answer is Only two:- Devichandragupta:-​It is a Sanskrit play written by Vishakhadatta. This narrates the heroic deeds of Chandragupta in which he rescues the wife of his elder brother from the Shaka ruler. Hence pair 1 is incorrect. Hammira Mahakavya:- Hammira Mahakavya is a 15th-century Indian Sanskrit epic poem written by the Jain scholar Nayachandra Suri. It is a legendary biography of the 13th-century Chahamana king Hammira. It tells about the Chahamana dynasty rulers. Hence pair 2 is correct. Milinda-panha:- It is the composition of dialogues between the Buddhist monk Nagasena and King Milinda. Hence pair 3 is incorrect. Milinda panha refers to (Questions of Milinda). It is written in the Pali language. Nitivakyamrita:- The Nitivakyamrita is a work on ethics, politics, and social norms written in Sanskrit. It was authored by Somadeva Suri, a renowned Jain monk and scholar. Hence pair 4 is correct.  The (Nītivākyāmr̥ta) is composed in the form of verses (shlokas) and contains teachings on various aspects of life including virtue, duty, conduct, and governance. It is an important work in the canon of Jain literature and is still studied for its profound insights into life and morality."
     }
 ];
 
@@ -142,6 +157,7 @@ console.log(checkedAnswer);
 if(checkedAnswer == quizDB[questionCount].ans){
     score++;
 };
+
 
 
 questionCount++;
