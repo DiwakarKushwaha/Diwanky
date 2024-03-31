9f4403f99e649c0ad92b6f99d51813369d9f4535
DiwakarKushwaha <99752642+DiwakarKushwaha@users.noreply.github.com>
2024-04-01 01:43:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const quizDB =[
         f:"",
         g:"",
         h:"",
+        i:"",
+        j:"",
         a:"Andhra",
         b:"Gandhara",
         c:"Kalinga",
@@ -18,6 +20,8 @@ const quizDB =[
         f:"",
         g:"",
         h:"",
+        i:"",
+        j:"",
         a:"capital cities",
         b:"ports",
         c:"centres of iron-and-steel making",
@@ -27,37 +31,43 @@ const quizDB =[
 
     },
     {
-        question: "In which one of the following regions was Dhanyakataka, which flourished as a prominent Buddhist centre under the Mahasanghikas, located?",
+        question: "Which one of the following explains the practice of Vattakirutal' as mentioned in Sangam poems?",
         e:"",
         f:"",
         g:"",
         h:"",
-        a:"Andhra",
-        b:"Gandhara",
-        c:"Kalinga",
-        d:"Magadha",
-        ans:"ans1",
+        i:"",
+        j:"",
+        a:"Kings employing women bodyguards.",
+        b:"Learned persons assembling in royal courts to discuss religious and philosophical matters",
+        c:"Young girls keeping watch over agricultural fields and driving away birds and animals",
+        d:"A king defeated in a battle committing ritual suicide by starving himself to death",
+        ans:"ans4",
         detail:""
     },
     {
-        question: "In which one of the following regions was Dhanyakataka, which flourished as a prominent Buddhist centre under the Mahasanghikas, located?",
+        question: "Consider the following dynasties:",
         e:"",
-        f:"",
-        g:"",
-        h:"",
-        a:"Andhra",
-        b:"Gandhara",
-        c:"Kalinga",
-        d:"Magadha",
-        ans:"ans1",
+        f:"1. Hoysala",
+        g:"2. Gahadavala",
+        h:"3. Kakatiya",
+        i:"4. Yadava",
+        j:"How many of the above dynasties established their kingdoms in early eighth century AD?",
+        a:"Only one",
+        b:"Only two",
+        c:"Only three",
+        d:"None",
+        ans:"ans4",
         detail:""
     },
     {
-        question: "In which one of the following regions was Dhanyakataka, which flourished as a prominent Buddhist centre under the Mahasanghikas, located?",
-        e:"",
+        question: "With reference to ancient Indian History, consider the following pairs:",
+        e:"Literary work&emsp; &emsp; &emsp; &emsp; &emsp;Author",
         f:"",
         g:"",
         h:"",
+        i:"",
+        j:"",
         a:"Andhra",
         b:"Gandhara",
         c:"Kalinga",
@@ -77,6 +87,13 @@ const submit = document.querySelector('#submit');
 const detailAnswer = document.querySelector('#detailAnswer');
 const answers = document.querySelectorAll('.answer');
 const showScore = document.querySelector('#showScore');
+const heading = document.querySelector('.heading');
+const point1 = document.querySelector('.point1');
+const point2 = document.querySelector('.point2');
+const point3 = document.querySelector('.point3');
+const point4 = document.querySelector('.point4');
+const point5 = document.querySelector('.point5');
+
 
 
 let questionCount = 0;
@@ -90,6 +107,16 @@ const loadQuestion = () => {
     option2.innerHTML = questionList.b;
     option3.innerHTML = questionList.c;
     option4.innerHTML = questionList.d;
+    heading.innerHTML = questionList.e;
+    point1.innerHTML = questionList.f;
+    point2.innerHTML = questionList.g;
+    point3.innerHTML = questionList.h;
+    point4.innerHTML = questionList.i;
+    point5.innerHTML = questionList.j;
+
+
+
+
 }   
 
 loadQuestion();
